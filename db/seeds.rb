434a0f4a10d1ccdfbc26fb359bcf917ae9cad2c9
Cesar Bongrand <mrbongrand@gmail.com>
2021-11-17 15:55:29 -0600

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  my_restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: %w[chinese italian japanese french belgian][rand(0..4)]
+  )
+  10.times do
+    review = Review.new(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5),
+      restaurant: my_restaurant
+    )
+    review.save!
+  end
+  my_restaurant.save!
+end
